21331bce93bd46fbd9770d4e14007db7076661ca
tartegauri <tartegauri30@gmail.com>
2024-04-11 12:58:29 +0530

    modified index.js and package.json for deployment

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-import listingRouter from "./routes/listing.route.js"
+import listingRouter from "./routes/listing.route.js";
+import path from "path"
 
 dotenv.config();
 mongoose
@@ -15,6 +16,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+  const _dirname = path.resolve();
+
 
 const app = express();
 
@@ -29,6 +33,11 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(_dirname, "/client/dist")));
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(_dirname, "client","dist","index.html"));
+})
 
 //middleware
 app.use((err, req, res, next) => {
